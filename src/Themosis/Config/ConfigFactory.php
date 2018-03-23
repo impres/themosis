4eb2435acfdc3c57a30263b9b769c17a23599614
Jelle <jelle@impres.nl>
2018-03-23 14:58:28 +0100

    added changes kevinkiel

--- src/Themosis/Config/ConfigFactory.php
+++ src/Themosis/Config/ConfigFactory.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Themosis\Config;
-
 class ConfigFactory implements IConfig
 {
     /**
@@ -10,12 +8,10 @@ class ConfigFactory implements IConfig
      * @var ConfigFinder
      */
     protected $finder;
-
     public function __construct(ConfigFinder $finder)
     {
         $this->finder = $finder;
     }
-
     /**
      * Return all or specific property from a config file.
      *
@@ -28,15 +24,31 @@ class ConfigFactory implements IConfig
         if (strpos($name, '.') !== false) {
             list($name, $property) = explode('.', $name);
         }
-
         $path = $this->finder->find($name);
         $properties = include $path;
-
         // Looking for single property
         if (isset($property) && isset($properties[$property])) {
             return $properties[$property];
         }
-
         return $properties;
+    }
+    /**
+     * Check if the config file exists.
+     *
+     * @param string $name The config file name or its property full name.
+     *
+     * @return bool
+     */
+    public function has($name)
+    {
+        if (strpos($name, '.') !== false) {
+            list($name, $property) = explode('.', $name);
+        }
+        foreach($this->finder->getPaths() as $path) {
+            if(file_exists($path . $name . '.config.php')) {
+                return true;
+            }
+        }
+        return false;
     }
 }
